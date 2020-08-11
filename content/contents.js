be1a02cfd66524372b734578a1ff6bb891174525
kokkiemouse <kokkiemouse@gmail.com>
2020-08-11 21:41:50 +0900

    added icon

--- content/contents.js
+++
@@ -1,8 +0,0 @@
-
-browser.runtime.onMessage.addListener(function(request, sender, sendResponse){
-    console.log(request.message);
-    sendResponse(document.location.href);
-    new QRCode(document.getElementById("qrcode"),document.location.href);
-    document.innerHTML="TDN";
-
-});
